81a618e858f6c6cd55b3b2936988f625eb0cdb75
Pavani <136723974+NerusuPavani37@users.noreply.github.com>
2025-05-26 17:59:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+function promiseFn(name,age){
+    return new Promise((resolve,reject) =>{
+      setTimeout(() =>{
+        if(age < 18){
+           reject(`Oh sorry ${name}. You aren't old enough.`)
+        }else{
+           resolve(`Welcome, ${name}. You can vote.`)
+        }
+      },4000)
+    });  
+}
+
+function handleSumbit(){
+  let form = document.querySelector('form');
+  let name = document.getElementById("name").value;
+  let age =parseInt(document.getElementById("age").value);
+  if(!name || isNaN(age)){
+    alert("Please enter valid details.");
+  }else{
+    promiseFn(name,age)
+    .then((mess) =>{
+      alert(mess);
+      form.reset();
+    })
+    .catch((err)=>{
+      alert(err);
+      form.reset();
+    })
+  }
+}
+
+document.getElementById('btn').addEventListener('click' , handleSumbit);
